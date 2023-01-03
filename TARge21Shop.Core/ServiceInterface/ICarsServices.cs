b8a1683a0bc09bd0cebcf14b01da313cef5e62c3
markiine <dmkotkas@gmail.com>
2023-01-03 14:48:00 +0200

    ICarsServices

---
+++ TARge21Shop.Core/ServiceInterface/ICarsServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TARge21Shop.Core.Domain;
+using TARge21Shop.Core.Dto;
+
+namespace TARge21Shop.Core.ServiceInterface
+{
+    public interface ICarsServices
+    {
+        Task<Spaceship> Create(CarDto dto);
+        Task<Spaceship> Update(CarDto dto);
+        Task<Spaceship> Delete(Guid id);
+        Task<Spaceship> GetAsync(Guid id);
+    }
+}
+
