cba8db87d1bdfb8b7dfb0d339e706af07b8c9676
markiine <dmkotkas@gmail.com>
2023-01-03 13:58:37 +0200

    Create Car.cs

---
+++ TARge21Shop.Core/Domain/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TARge21Shop.Core.Domain
+{
+    public class Car
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+        public int HorsePower { get; set; }
+        public int Weight { get; set; }
+        public DateTime BuiltDate { get; set; }
+        public DateTime LastMaintenance { get; set; }
+
+        // only in database
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
+
