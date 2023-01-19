28c7cb1057868b20eebbb51866172a84ddca3cbd
Opilane TTHK <opilane@tthk.ee>
2023-01-19 11:04:22 +0200

    update remove image button

--- TARge21Shop/Controllers/SpaceshipsController.cs
+++ TARge21Shop/Controllers/SpaceshipsController.cs
@@ -217,23 +217,34 @@ namespace TARge21Shop.Controllers
                 return NotFound();
             }
 
-            var vm = new SpaceshipDeleteViewModel()
-            {
-                Id = spaceship.Id,
-                Name = spaceship.Name,
-                Type = spaceship.Type,
-                Crew = spaceship.Crew,
-                Passengers = spaceship.Passengers,
-                CargoWeight = spaceship.CargoWeight,
-                FullTripsCount = spaceship.FullTripsCount,
-                MaintenanceCount = spaceship.MaintenanceCount,
-                LastMaintenance = spaceship.LastMaintenance,
-                EnginePower = spaceship.EnginePower,
-                MaidenLaunch = spaceship.MaidenLaunch,
-                BuiltDate = spaceship.BuiltDate,
-                CreatedAt = spaceship.CreatedAt,
-                ModifiedAt = spaceship.ModifiedAt
-            };
+            var photos = await _context.FileToDatabases
+                .Where(x => x.SpaceshipId == id)
+                .Select(y => new ImageViewModel
+                {
+                    SpaceshipId = y.Id,
+                    ImageId = y.Id,
+                    ImageData = y.ImageData,
+                    ImageTitle = y.ImageTitle,
+                    Image = string.Format("data:image/gif;base64,{0}", Convert.ToBase64String(y.ImageData))
+                }).ToArrayAsync();
+
+            var vm = new SpaceshipDeleteViewModel();
+            
+            vm.Id = spaceship.Id;
+            vm.Name = spaceship.Name;
+            vm.Type = spaceship.Type;
+            vm.Crew = spaceship.Crew;
+            vm.Passengers = spaceship.Passengers;
+            vm.CargoWeight = spaceship.CargoWeight;
+            vm.FullTripsCount = spaceship.FullTripsCount;
+            vm.MaintenanceCount = spaceship.MaintenanceCount;
+            vm.LastMaintenance = spaceship.LastMaintenance;
+            vm.EnginePower = spaceship.EnginePower;
+            vm.MaidenLaunch = spaceship.MaidenLaunch;
+            vm.BuiltDate = spaceship.BuiltDate;
+            vm.CreatedAt = spaceship.CreatedAt;
+            vm.ModifiedAt = spaceship.ModifiedAt;
+            vm.Image.AddRange(photos);
 
             return View(vm);
         }
