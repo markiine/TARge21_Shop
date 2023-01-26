343e471f6d5014c2c523ab9f8a5085c1f870cf56
Opilane TTHK <opilane@tthk.ee>
2023-01-26 10:38:19 +0200

    IRealEstates

--- TARge21Shop.Core/ServiceInterface/IRealEstates.cs
+++ TARge21Shop.Core/ServiceInterface/IRealEstates.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TARge21Shop.Core.Domain;
+using TARge21Shop.Core.Dto;
 
 namespace TARge21Shop.Core.ServiceInterface
 {
-    internal interface IRealEstates
+    public interface IRealEstates
     {
+        //Task<RealEstate> Create(RealEstateDto dto);
+        //Task<RealEstate> Update(RealEstateDto dto);
+        //Task<RealEstate> Delete(Guid id);
+        Task<RealEstate> GetAsync(Guid id);
     }
 }
