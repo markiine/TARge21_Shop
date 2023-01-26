9f01acd09bfe003a7e4216303cc36b638314e206
Opilane TTHK <opilane@tthk.ee>
2023-01-26 10:37:52 +0200

    interface

--- TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
+++ TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TARge21Shop.Data;
 
 namespace TARge21Shop.ApplicationServices.Services
 {
     public class RealEstatesServices
     {
+        private readonly TARge21ShopContext _context;
+        public RealEstatesServices
+            (
+                TARge21ShopContext context
+            )
+        {
+            _context = context;
+        }
 
     }
 }
