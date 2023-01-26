6998a2ebc20a796aa69966c13be857519f7dc52e
Opilane TTHK <opilane@tthk.ee>
2023-01-26 10:27:18 +0200

    RealEstate basic

--- TARge21Shop.Data/TARge21ShopContext.cs
+++ TARge21Shop.Data/TARge21ShopContext.cs
@@ -11,11 +11,11 @@ namespace TARge21Shop.Data
     public class TARge21ShopContext : DbContext
     {
         public TARge21ShopContext(DbContextOptions<TARge21ShopContext> options) : base(options)
-        {
-        }
+        {}
 
         public DbSet<Spaceship> Spaceships { get; set; }
         public DbSet<FileToDatabase> FileToDatabases { get; set; }
+        public DbSet<RealEstate> RealEstates { get; set; }
         
     }
 }
