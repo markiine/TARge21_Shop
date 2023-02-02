3a5127a9f9b63790929e0f3cdf7eed48597b1cc1
Opilane TTHK <opilane@tthk.ee>
2023-02-02 09:57:44 +0200

    Update RealEstateServices

--- TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
+++ TARge21Shop.ApplicationServices/Services/RealEstatesServices.cs
@@ -15,12 +15,15 @@ namespace TARge21Shop.ApplicationServices.Services
     public class RealEstatesServices : IRealEstatesServices
     {
         private readonly TARge21ShopContext _context;
+        // private readonly IFilesServices _files;
         public RealEstatesServices
             (
                 TARge21ShopContext context
+                //IFilesServices files
             )
         {
             _context = context;
+            //_files = files;
         }
 
         public async Task<RealEstate> GetAsync(Guid id)
@@ -34,6 +37,7 @@ namespace TARge21Shop.ApplicationServices.Services
         public async Task<RealEstate> Create(RealEstateDto dto)
         {
             RealEstate realEstate = new();
+            //FileToDatabase file = new FileToDatabase();
 
             realEstate.Id = Guid.NewGuid();
             realEstate.Address = dto.Address;
@@ -50,6 +54,12 @@ namespace TARge21Shop.ApplicationServices.Services
             realEstate.ModifiedAt = DateTime.Now;
             realEstate.CreatedAt = DateTime.Now;
 
+            //if (dto.Files != null)
+            //{
+            //    _files.UploadFilesToDatabase(dto, realEstate);
+            //}
+
+
             await _context.RealEstates.AddAsync(realEstate);
             await _context.SaveChangesAsync();
 
@@ -58,58 +68,58 @@ namespace TARge21Shop.ApplicationServices.Services
         }
 
 
-        /*public async Task<Spaceship> Update(SpaceshipDto dto)
+        public async Task<RealEstate> Update(RealEstateDto dto)
         {
-            var domain = new Spaceship()
+            var domain = new RealEstate()
             {
-                Id = dto.Id,
-                Name = dto.Name,
-                Type = dto.Type,
-                Crew = dto.Crew,
-                Passengers = dto.Passengers,
-                CargoWeight = dto.CargoWeight,
-                FullTripsCount = dto.FullTripsCount,
-                MaintenanceCount = dto.MaintenanceCount,
-                LastMaintenance = dto.LastMaintenance,
-                EnginePower = dto.EnginePower,
-                MaidenLaunch = dto.MaidenLaunch,
-                BuiltDate = dto.BuiltDate,
-                CreatedAt = dto.CreatedAt,
-                ModifiedAt = DateTime.Now
+                 Id = Guid.NewGuid(),
+                Address = dto.Address,
+                City = dto.City,
+                Region = dto.Region,
+                PostalCode = dto.PostalCode,
+                Country = dto.Country,
+                Phone = dto.Phone,
+                Fax = dto.Fax,
+                Size = dto.Size,
+                Floor = dto.Floor,
+                Price = dto.Price,
+                RoomCount = dto.RoomCount,
+                ModifiedAt = DateTime.Now,
+                CreatedAt = dto.CreatedAt
             };
 
-            if (dto.Files != null)
-            {
-                _files.UploadFilesToDatabase(dto, domain);
-            }
+            //if (dto.Files != null)
+            //{
+            //    _files.UploadFilesToDatabase(dto, domain);
+            //}
 
-            _context.Spaceships.Update(domain);
+            _context.RealEstates.Update(domain);
             await _context.SaveChangesAsync();
 
             return domain;
         }
 
-        public async Task<Spaceship> Delete(Guid id)
+        public async Task<RealEstate> Delete(Guid id)
         {
-            var spaceshipId = await _context.Spaceships
+            var realEstateId = await _context.RealEstates
                 .FirstOrDefaultAsync(x => x.Id == id);
 
-            var images = await _context.FileToDatabases
-                .Where(x => x.SpaceshipId == id)
-                .Select(y => new FileToDatabaseDto
-                {
-                    Id = y.Id,
-                    ImageTitle = y.ImageTitle,
-                    SpaceshipId = y.SpaceshipId,
-                })
-                .ToArrayAsync();
+            //var images = await _context.FileToDatabases
+            //    .Where(x => x.RealEstateId == id)
+            //    .Select(y => new FileToDatabaseDto
+            //    {
+            //        Id = y.Id,
+            //        ImageTitle = y.ImageTitle,
+            //        RealEstateId = y.RealEstateId,
+            //    })
+            //    .ToArrayAsync();
 
-            await _files.RemoveImagesFromDatabase(images); // et andmebaasist ka ära kustuks
-            _context.Spaceships.Remove(spaceshipId);
-            await _context.SaveChangesAsync();
+            //await _files.RemoveImagesFromDatabase(images); // et andmebaasist ka ära kustuks
+            //_context.RealEstates.Remove(realEstateId);
+            //await _context.SaveChangesAsync();
 
-            return spaceshipId;
-        }*/
+            return realEstateId;
+        }
 
     }
 }
