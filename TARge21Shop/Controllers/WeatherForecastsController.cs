3b404c77ee43b22a532098841b315062afb7d056
Opilane TTHK <opilane@tthk.ee>
2023-03-02 10:04:29 +0200

    Update WeatherForecastsController.cs

--- TARge21Shop/Controllers/WeatherForecastsController.cs
+++ TARge21Shop/Controllers/WeatherForecastsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Core.Dto.WeatherDtos;
 using TARge21Shop.Core.ServiceInterface;
 using TARge21Shop.Models.Weather;
 
@@ -15,11 +16,52 @@ namespace TARge21Shop.Controllers
         {
             _weatherForecastsServices = weatherForecastsServices;
         }
+        
         public IActionResult Index()
         {
             WeatherViewModel vm = new WeatherViewModel();
 
             return View();
         }
+
+        [HttpPost]
+        public IActionResult ShowWeather(WeatherViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "WeatherForecasts");
+            }
+
+            return View(vm);
+        }        
+        
+        [HttpGet]
+        public IActionResult City()
+        {
+            WeatherResultDto dto = new();
+            WeatherViewModel vm = new();
+
+            _weatherForecastsServices.WeatherDetail(dto);
+
+            vm.Date = dto.EffectiveDate;
+            vm.EpochDate = dto.EffectiveEpochDate;
+            vm.Severity = dto.Severity;
+            vm.Text = dto.Text;
+            vm.Category = dto.Category;
+            vm.MobileLink = dto.MobileLink;
+            vm.Link = dto.Link;
+
+
+            vm.Temperature.Maximum.Value = dto.MaximumValue;
+            vm.Temperature.Maximum.Unit = dto.MaximumUnit;
+            vm.Temperature.Maximum.UnitType = dto.MaximumUnitType;
+
+            vm.Temperature.Minimum.Value = dto.MinimumValue;
+            vm.Temperature.Minimum.Unit = dto.MinimumUnit;
+            vm.Temperature.Minimum.UnitType = dto.MinimumUnitType;
+
+            return View();
+        }
+
     }
 }
