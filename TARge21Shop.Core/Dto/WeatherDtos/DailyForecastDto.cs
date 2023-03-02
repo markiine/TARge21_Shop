8de0904318dc47acc19786404c94818d11f6f928
Opilane TTHK <opilane@tthk.ee>
2023-03-02 11:26:33 +0200

    Add json properties, view labels

--- TARge21Shop.Core/Dto/WeatherDtos/DailyForecastDto.cs
+++ TARge21Shop.Core/Dto/WeatherDtos/DailyForecastDto.cs
@@ -2,58 +2,106 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TARge21Shop.Core.Dto.WeatherDtos
 {
     public class DailyForecastDto
     {
+        [JsonPropertyName("Date")]
         public DateTime Date { get; set; }
+
+        [JsonPropertyName("EpochDate")]
         public int EpochDate { get; set; }
+
+        [JsonPropertyName("Temperature")]
         public Temperature Temperature { get; set; }
+
+        [JsonPropertyName("Day")]
         public Day Day { get; set; }
+
+        [JsonPropertyName("Night")]
         public Night Night { get; set; }
+
+        [JsonPropertyName("Sources")]
         public List<string> Sources { get; set; }
+
+        [JsonPropertyName("MobileLink")]
         public string MobileLink { get; set; }
+
+        [JsonPropertyName("Link")]
         public string Link { get; set; }
+
     }
 
     public class Day
     {
+        [JsonPropertyName("Icon")]
         public int Icon { get; set; }
+
+        [JsonPropertyName("IconPhrase")]
         public string IconPhrase { get; set; }
+
+        [JsonPropertyName("HasPrecipitation")]
         public bool HasPrecipitation { get; set; }
+
+        [JsonPropertyName("PrecipitationType")]
         public string PrecipitationType { get; set; }
+
+        [JsonPropertyName("PrecipitationIntensity")]
         public string PrecipitationIntensity { get; set; }
 
     }
     public class Night
     {
+        [JsonPropertyName("Icon")]
         public int Icon { get; set; }
+
+        [JsonPropertyName("IconPhrase")]
         public string IconPhrase { get; set; }
+
+        [JsonPropertyName("HasPrecipitation")]
         public bool HasPrecipitation { get; set; }
+
+        [JsonPropertyName("PrecipitationType")]
         public string PrecipitationType { get; set; }
+
+        [JsonPropertyName("PrecipitationIntensity")]
         public string PrecipitationIntensity { get; set; }
     }
 
 
     public class Maximum
     {
+        [JsonPropertyName("Value")]
         public double Value { get; set; }
+
+        [JsonPropertyName("Unit")]
         public string Unit { get; set; }
+
+        [JsonPropertyName("UnitType")]
         public int UnitType { get; set; }
     }
 
     public class Minimum
     {
+        [JsonPropertyName("Value")]
         public double Value { get; set; }
+
+        [JsonPropertyName("Unit")]
         public string Unit { get; set; }
+
+        [JsonPropertyName("UnitType")]
         public int UnitType { get; set; }
     }
 
     public class Temperature
     {
+        [JsonPropertyName("Minimum")]
         public Minimum Minimum { get; set; }
+
+        [JsonPropertyName("Maximum")]
         public Maximum Maximum { get; set; }
     }
 
