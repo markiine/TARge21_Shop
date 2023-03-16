30a2fa39e397e1e61e002fdc134533b076104ac2
markiine <dmkotkas@gmail.com>
2023-03-16 03:08:12 +0200

    Add view and model

---
+++ TARge21Shop/Models/Weather/OpenWeatherViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TARge21Shop.Models.Weather
+{
+    public class OpenWeatherViewModel
+    {
+        public string Base { get; set; }
+        public int Visibility { get; set; }
+        public int Dt { get; set; }
+        public int Timezone { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
+        public Coords Coord { get; set; }
+        public Weathers Weather { get; set; }
+        public Mains Main { get; set; }
+        public Winds Wind { get; set; }
+        public Rains Rain { get; set; }
+        public Clouds Clouds { get; set; }
+        public Sys Sys { get; set; }
+    }
+
+    public class Coords
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class Weathers
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Mains
+    {
+        public double Temp { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public int SeaLevel { get; set; }
+        public int GroundLevel { get; set; }
+    }
+
+    public class Winds
+    {
+        public double WindSpeed { get; set; }
+        public int Degree { get; set; }
+        public double Gust { get; set; }
+    }
+
+    public class Rains
+    {
+        public double RainProblability { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Type { get; set; }
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public int Sunrise { get; set; }
+        public int SunSet { get; set; }
+    }
+    
+}
