e07effabb5fd5ee3d1d5d3aaa1adac022fddcb7e
markiine <dmkotkas@gmail.com>
2023-03-16 02:05:49 +0200

    Add task to service interface

--- TARge21Shop.Core/ServiceInterface/IWeatherForecastsServices.cs
+++ TARge21Shop.Core/ServiceInterface/IWeatherForecastsServices.cs
@@ -5,5 +5,6 @@ namespace TARge21Shop.Core.ServiceInterface
     public interface IWeatherForecastsServices
     {
         Task<WeatherResultDto> WeatherDetail(WeatherResultDto dto);
+        Task<OpenWeatherResultDto> OpenWeatherDetail(OpenWeatherResultDto dto);
     }
 }
