f959fae23c9ad41e829d65838825081f6e57a7cc
markiine <dmkotkas@gmail.com>
2023-03-16 17:10:20 +0200

    Remove unnecessary usings

--- TARge21Shop.SpaceshipTest/SpaceshipTest.cs
+++ TARge21Shop.SpaceshipTest/SpaceshipTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TARge21Shop.Core.Dto;
+﻿using TARge21Shop.Core.Dto;
 using TARge21Shop.Core.ServiceInterface;
 
 namespace TARge21Shop.SpaceshipTest
